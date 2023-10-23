d53cc00a6c175a3f583fe9da4833dd8504903e8e
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-23 23:14:38 +0300

    Fix(JSON) Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'react-app/jest',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Ваши собственные настройки правил здесь
+  },
+};
